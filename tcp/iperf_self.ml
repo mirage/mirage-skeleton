193e1d2ce4c3a7923be18fc2c9e16bc39a5a5318
Vincent Bernardoff <vincent.bernardoff@citrix.com>
2013-07-01 12:17:24 +0100

    Updated iperf_self test to match the one in mirage-net.

--- tcp/iperf_self.ml
+++ tcp/iperf_self.ml
@@ -15,7 +15,7 @@
  * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  *)
 
-open Lwt
+open Lwt 
 open Printf
 open OS.Clock
 open Gc
@@ -52,38 +52,36 @@ let msg = "01234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ012345
 
 let mlen = String.length msg
 
-let iperfclient tt ip =
-  OS.Time.sleep 2. >>
+let iperfclient mgr src_ip dest_ip dport =
+  let iperftx chan =
+    printf "Iperf client: Made connection to server. \n%!";
+    let a = Cstruct.sub (OS.Io_page.(to_cstruct (get 1))) 0 mlen in
+    Cstruct.blit_from_string msg 0 a 0 mlen;
+    let amt = 1000000000 in
+    for_lwt i = (amt / mlen) downto 1 do
+      Net.Flow.write chan a
+    done >>
+    let a = Cstruct.sub a 0 (amt - (mlen * (amt/mlen))) in
+    Net.Flow.write chan a >>
+    Net.Flow.close chan
+  in
+  OS.Time.sleep 5. >>
   (printf "Iperf client: Attempting connection. \n%!";
-   lwt conn = Net.Tcp.Pcb.connect tt ~dest_ip:ip ~dest_port:port in
-   match conn with
-   | None ->
-       printf "Iperf client: Unable to connect to remote host (is the iperf server up?) \n%!";
-       return ()
-   | Some (pcb, _) ->
-       printf "Iperf client: Made connection to server. \n%!";
-       let a_io = OS.Io_page.get () in
-       let a = OS.Io_page.to_cstruct a_io in
-       Cstruct.blit_from_string msg 0 a 0 mlen;
-       let a = Cstruct.sub a 0 mlen in
-       let amt = 1000000000 in
-       for_lwt i = (amt / mlen) downto 1 do
-         Net.Tcp.Pcb.write pcb a
-       done >>
-       let a = Cstruct.sub a 0 (amt - (mlen * (amt/mlen))) in
-       Net.Tcp.Pcb.write pcb a >>
-       (printf "Iperf client: Done.\n%!";
-	Net.Tcp.Pcb.close pcb)
+   lwt conn = Net.Flow.connect mgr (`TCPv4 (Some (Some src_ip, 0),
+					    (dest_ip, dport), iperftx)) in
+   printf "Iperf client: Done.\n%!";
+   return ()
   )
 
-let print_data st ts_now =
+
+let print_data st ts_now = 
   Printf.printf "Iperf server: t = %f, rate = %Ld KBits/s, totbytes = %Ld, live_words = %d\n%!"
     (ts_now -. st.start_time)
     (Int64.of_float (((Int64.to_float st.bin_bytes) /. (ts_now -. st.last_time)) /. 125.))
-    st.bytes Gc.((stat()).live_words);
+    st.bytes Gc.((stat()).live_words); 
   st.last_time <- ts_now;
   st.bin_bytes <- 0L;
-  st.bin_packets <- 0L
+  st.bin_packets <- 0L 
 
 
 let iperf (dip,dpt) chan =
@@ -93,7 +91,7 @@ let iperf (dip,dpt) chan =
   let rec iperf_h chan =
     match_lwt Net.Flow.read chan with
     | None ->
-	let ts_now = (OS.Clock.time ()) in
+	let ts_now = (OS.Clock.time ()) in 
 	st.bin_bytes <- st.bytes;
 	st.bin_packets <- st.packets;
 	st.last_time <- st.start_time;
@@ -106,7 +104,7 @@ let iperf (dip,dpt) chan =
 	st.packets <- (Int64.add st.packets 1L);
 	st.bin_bytes <- (Int64.add st.bin_bytes (Int64.of_int l));
 	st.bin_packets <- (Int64.add st.bin_packets 1L);
-	let ts_now = (OS.Clock.time ()) in
+	let ts_now = (OS.Clock.time ()) in 
 	if ((ts_now -. st.last_time) >= 1.0) then begin
           print_data st ts_now;
 	end;
@@ -116,28 +114,25 @@ let iperf (dip,dpt) chan =
   iperf_h chan
 
 
-let main _ _ _ =
-  Net.Manager.create (fun mgr interface id ->
-    let intfnum = int_of_string id in
-    match intfnum with
-    | 0 ->
-	OS.Time.sleep 2. >>
-	(printf "Setting up iperf client on interface %s\n%!" id;
-	 Net.Manager.configure interface (`IPv4 ip2) >>
-	 let tcps = Net.Manager.tcpv4_of_addr mgr None in
-	 let tt = List.hd tcps in
-	 let (ip,_,_) = ip1 in
-	 lwt () = iperfclient tt ip in
-         return ()
-	)
-    | 1 ->
-	OS.Time.sleep 1. >>
-	(printf "Setting up iperf server on interface %s\n%!" id;
-	 Net.Manager.configure interface (`IPv4 ip1) >>
-	 let _ = Net.Flow.listen mgr (`TCPv4 ((None, port), iperf)) in
-	 printf "Done setting up server \n%!";
-	 return ()
-	)
-    | _ ->
-	(printf "interface %s not used\n%!" id; return ())
-  )
+let main mgr interface id =
+  let intfnum = int_of_string id in
+  match intfnum with
+  | 0 ->
+	  OS.Time.sleep 2. >>
+	  (printf "Setting up iperf client on interface %s\n%!" id;
+	   Net.Manager.configure interface (`IPv4 ip2) >>
+	   let (src_ip,_,_) = ip2 in
+	   let (dest_ip,_,_) = ip1 in
+	   iperfclient mgr src_ip dest_ip port >>
+	   return()
+	  )
+  | 1 ->
+	  OS.Time.sleep 1. >>
+	  (printf "Setting up iperf server on interface %s\n%!" id;
+	   Net.Manager.configure interface (`IPv4 ip1) >>
+	   let _ = Net.Flow.listen mgr (`TCPv4 ((None, port), iperf)) in
+	   printf "Done setting up server \n%!";
+	   return ()
+	  )
+  | _ ->
+	  (printf "interface %s not used\n%!" id; return ())
