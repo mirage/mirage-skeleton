f84fec2071e26424f33cab4ff140f2827a08d729
Richard Mortier <mort@cantab.net>
2013-08-11 18:25:06 -0400

    [basic] hello world needs no network"

--- basic/hello.ml
+++ basic/hello.ml
@@ -1,7 +1,7 @@
-let main _ _ _ =
-  for_lwt i = 0 to 10 do
-    print_endline "hello";
+let main () =
+  for_lwt i = 0 to 4 do
+    print_endline "hello" ;
     lwt () = OS.Time.sleep 2.0 in
-    print_endline "world";
+    print_endline "world" ;
     Lwt.return ()
   done
