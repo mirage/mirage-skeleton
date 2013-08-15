f377fede224ee0ebc4f8104b948598aa0f74589b
Richard Mortier <mort@cantab.net>
2013-08-15 11:38:28 -0400

    [lwt] add second challenge code and conf

--- lwt/src/answers.ml
+++ lwt/src/answers.ml
@@ -12,3 +12,12 @@ let one () =
   ]) (fun () ->
     print_endline ("Finished"); return ()
   )
+
+let two () =
+  join [
+    (Time.sleep 1.0 >>= fun () -> (Console.log "Heads"; return ()));
+    (Time.sleep 2.0 >>= fun () -> (Console.log "Tails"; return ()));
+  ] >>= (fun () ->
+    Console.log "Finished";
+    return ()
+  )
