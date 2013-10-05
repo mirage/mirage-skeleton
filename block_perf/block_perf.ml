a40e8c893c2d6cc5609da1a1caf3a7a11e494f7b
David Scott <dave.scott@eu.citrix.com>
2013-10-05 12:04:30 +0100

    block_perf: add a watchdog thread
    Detect deadlocks and print an error. Periodically dump the state
    of each event channel.
    
    Signed-off-by: David Scott <dave.scott@eu.citrix.com>

--- block_perf/block_perf.ml
+++ block_perf/block_perf.ml
@@ -20,14 +20,28 @@ let main _ =
   printf "Connected block device\n%!";
   printf "Total device size = %Ld\nSector size = %d\n%!" blkif#size blkif#sector_size;
   printf "Device is read%s\n%!" (if blkif#readwrite then "/write" else "-only");
+  let last_read = ref (OS.Clock.time ()) in
   let module M = struct
     let page_size_bytes = 4096
     let sector_size = 512
     let sectors_per_page = page_size_bytes / sector_size
-    let read_sectors (start, length) = Int64.(blkif#read_512 (of_int start) (of_int length))
+    let read_sectors (start, length) =
+      last_read := OS.Clock.time ();
+      Int64.(blkif#read_512 (of_int start) (of_int length))
     let gettimeofday = OS.Clock.time
   end in
   let module Test = Perf.Test(M) in
+  printf "starting in 3s\n%!";
+  lwt () = OS.Time.sleep 3.0 in
+  let watchdog_t =
+    while_lwt true do
+      lwt () = OS.Time.sleep 10.0 in
+      let now = OS.Clock.time () in
+      if now -. !last_read > 10.
+      then printf "** more than 10s since last read request: deadlock?\n%!";
+      OS.Activations.dump ();
+      return ()
+    done in
   lwt results = Test.go (Int64.to_int blkif#size) in
 
 (* Write the results to the first page on the disk, unused for now:
