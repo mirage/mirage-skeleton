04dc6655f386b55e7b58cc23c404811f29e00f93
Richard Mortier <mort@cantab.net>
2014-02-02 11:37:39 +0100

    [fosdem] bug: clock already included, -unix prevents xen build

--- ethifv4/config.ml
+++ ethifv4/config.ml
@@ -4,9 +4,7 @@ let main = foreign "Unikernel.Main" (console @-> network @-> job)
 
 let () =
   add_to_ocamlfind_libraries 
-    [ "mirage-clock-unix";
-      "tcpip.ethif"; "tcpip.tcpv4"; "tcpip.udpv4"; "tcpip.dhcpv4"
-    ];
+    [ "tcpip.ethif"; "tcpip.tcpv4"; "tcpip.udpv4"; "tcpip.dhcpv4" ];
   register "ethifv4" [
     main $ default_console $ tap0
   ]
