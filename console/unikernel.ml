9b738b8a089614607b815b178cb9cf522882b407
Anil Madhavapeddy <anil@recoil.org>
2014-02-10 16:46:29 +0000

    sync console example with wiki

--- console/unikernel.ml
+++ console/unikernel.ml
@@ -3,7 +3,7 @@ open Lwt
 module Main (C: V1_LWT.CONSOLE) = struct
 
   let start c =
-    for_lwt i = 0 to 2 do
+    for_lwt i = 0 to 4 do
       C.log c "hello" ;
       lwt () = OS.Time.sleep 1.0 in
       C.log c "world" ;
