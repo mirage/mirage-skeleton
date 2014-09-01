69bc26048487ff9727acff5ec93ace8807a36397
Anil Madhavapeddy <anil@recoil.org>
2014-09-01 09:46:47 +0200

    HTTP now uses a Conduit functor argument, not Stack

--- stackv4/unikernel.ml
+++ stackv4/unikernel.ml
@@ -9,13 +9,19 @@ let blue fmt   = sprintf ("\027[36m"^^fmt^^"\027[m")
 
 module Main (C:CONSOLE) (S:STACKV4) = struct
 
-  module T  = S.TCPV4
-  module CH = Channel.Make(T)
-  module H  = HTTP.Make(CH)
+  module DNS = Dns_resolver_mirage.Make(OS.Time)(S)
+  module RES = Conduit_resolver_mirage.Make(DNS)
+  module CON = Conduit_mirage.Make(S)
+  module H   = HTTP.Make(CON)
 
   let start console s =
 
-    C.log_s console (sprintf "IP address: %s\n" (Ipaddr.V4.to_string (S.IPV4.get_ipv4 (S.ipv4 s)))) >>= fun () ->
+    C.log_s console (sprintf "IP address: %s\n"
+      (Ipaddr.V4.to_string (S.IPV4.get_ipv4 (S.ipv4 s))))
+    >>= fun () ->
+
+    lwt ctx = CON.init s in
+
     let http_callback conn_id req body =
       let path = Uri.path (H.Server.Request.uri req) in
       C.log_s console (sprintf "Got request for %s\n" path) 
@@ -28,33 +34,8 @@ module Main (C:CONSOLE) (S:STACKV4) = struct
       conn_closed = fun _ () -> ();
     } in
 
-    S.listen_udpv4 s 53 (
-      fun ~src ~dst ~src_port buf ->
-        C.log_s console "got udp on 53"
-    );
+    CON.serve ~ctx ~mode:(`TCP (`Port 80)) (H.Server.listen spec)
 
-    S.listen_tcpv4 s 8080 (
-      fun flow ->
-        let dst, dst_port = T.get_dest flow in
-        C.log_s console
-          (green "new tcp from %s %d" 
-            (Ipaddr.V4.to_string dst) dst_port
-          )
-        >>= fun () ->
-        T.read flow
-        >>= function
-        | `Ok b ->
-          C.log_s console
-            (yellow "read: %d\n%s" 
-              (Cstruct.len b) (Cstruct.to_string b)
-            )
-          >>= fun () ->
-          T.close flow
-        | `Eof -> C.log_s console (red "read: eof")
-        | `Error e -> C.log_s console (red "read: error")
-    );
-
-    S.listen_tcpv4 s 80 (H.Server.listen spec);
-    S.listen s
-
+    >>= fun () ->
+    OS.Time.sleep 100000. (* TODO fixme by making CON.serve block *)
 end
