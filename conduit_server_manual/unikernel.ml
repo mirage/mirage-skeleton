16f63a97623f2fb1822796768b4857b1792dfe56
David Scott <dave.scott@eu.citrix.com>
2014-11-02 14:31:56 +0000

    Fix the conduit_server_manual example
    This now builds with the current mirage-xen dev packages.
    
    Signed-off-by: David Scott <dave.scott@eu.citrix.com>

--- conduit_server_manual/unikernel.ml
+++ conduit_server_manual/unikernel.ml
@@ -10,8 +10,8 @@ let blue fmt   = sprintf ("\027[36m"^^fmt^^"\027[m")
 module Main (C:CONSOLE) (S:STACKV4) = struct
 
   module DNS = Dns_resolver_mirage.Make(OS.Time)(S)
-  module RES = Conduit_resolver_mirage.Make(DNS)
-  module CON = Conduit_mirage.Make(S)(Vchan.In_memory)
+  module RES = Resolver_mirage.Make(DNS)
+  module CON = Conduit_mirage.Make(S)(Conduit_xenstore)
   module H   = HTTP.Make(CON)
 
   let start console s =
@@ -20,7 +20,7 @@ module Main (C:CONSOLE) (S:STACKV4) = struct
       (Ipaddr.V4.to_string (S.IPV4.get_ipv4 (S.ipv4 s))))
     >>= fun () ->
 
-    lwt ctx = CON.init s in
+    lwt ctx = CON.init () in
 
     let http_callback conn_id req body =
       let path = Uri.path (H.Server.Request.uri req) in
