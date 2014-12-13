9fc5c81d51d484112593357cb8ec304b85767d60
Nicolas Ojeda Bar <n.oje.bar@gmail.com>
2014-12-13 11:29:42 -0300

    Add Ipv6 version of `ping`

---
+++ ping6/config.ml
@@ -0,0 +1,8 @@
+open Mirage
+
+let main = foreign "Unikernel.Main" (console @-> network @-> clock @-> job)
+
+let () =
+  add_to_opam_packages [ "tcpip" ];
+  add_to_ocamlfind_libraries [ "tcpip.ethif"; "tcpip.ipv6" ];
+  register "ping" [ main $ default_console $ tap0 $ default_clock ]
