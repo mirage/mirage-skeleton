f0292da11ea73f46b7cc37dfbba266ca5b0d6536
Nicolas Ojeda Bar <n.oje.bar@gmail.com>
2014-12-13 11:37:34 -0300

    ping: add `tcpip` as opam dependency

--- ping/config.ml
+++ ping/config.ml
@@ -3,5 +3,6 @@ open Mirage
 let main = foreign "Unikernel.Main" (console @-> network @-> job)
 
 let () =
+  add_to_opam_packages [ "tcpip" ];
   add_to_ocamlfind_libraries [ "tcpip.ethif"; "tcpip.ipv4" ];
   register "ping" [ main $ default_console $ tap0 ]
