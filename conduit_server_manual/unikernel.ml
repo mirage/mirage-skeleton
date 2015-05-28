a3f036d2e0c33800cbea8edb476fab5026c28931
Thomas Gazagnaire <thomas@gazagnaire.org>
2015-05-28 11:30:05 +0000

    Fix conduit-server-manual to use latest Conduit API

--- conduit_server_manual/unikernel.ml
+++ conduit_server_manual/unikernel.ml
@@ -11,8 +11,10 @@ module Main (C:CONSOLE) (S:STACKV4) = struct
 
   module DNS = Dns_resolver_mirage.Make(OS.Time)(S)
   module RES = Resolver_mirage.Make(DNS)
-  module CON = Conduit_mirage
-  module H   = Cohttp_mirage.Server(CON.Flow)
+  module H   = Cohttp_mirage.Server(Conduit_mirage.Flow)
+
+  let conduit = Conduit_mirage.empty
+  let stackv4 = Conduit_mirage.stackv4 (module S)
 
   let start console s =
 
@@ -20,7 +22,7 @@ module Main (C:CONSOLE) (S:STACKV4) = struct
       (String.concat ", " (List.map Ipaddr.V4.to_string (S.IPV4.get_ip (S.ipv4 s)))))
     >>= fun () ->
 
-    CON.with_tcp CON.empty (module S) s >>= fun conduit ->
+    Conduit_mirage.with_tcp conduit stackv4 s >>= fun conduit ->
 
     let http_callback conn_id req body =
       let path = Uri.path (H.Request.uri req) in
@@ -30,5 +32,5 @@ module Main (C:CONSOLE) (S:STACKV4) = struct
     in
 
     let spec = H.make ~callback:http_callback () in
-    CON.listen conduit(`TCP 80) (H.listen spec)
+    Conduit_mirage.listen conduit(`TCP 80) (H.listen spec)
 end
