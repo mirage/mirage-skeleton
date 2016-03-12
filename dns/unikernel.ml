b0606c3f3a703e8723b273a19e27210bcc892a08
Thomas Gazagnaire <thomas@gazagnaire.org>
2016-03-12 20:59:48 +0000

    dns: remove syntax extensions

--- dns/unikernel.ml
+++ dns/unikernel.ml
@@ -1,4 +1,4 @@
-open Lwt
+open Lwt.Infix
 open V1_LWT
 open Printf
 
@@ -16,16 +16,16 @@ module Main (C:CONSOLE) (K:KV_RO) (S:STACKV4) = struct
   (** Note that a lot of this logic will eventually move into
       a mirage-dns library, and is just here temporarily *)
   let start c k s =
-    lwt zonebuf =
-      K.size k "test.zone"
-      >>= function
-      | `Error _ -> fail (Failure "test.zone not found")
+    begin
+      K.size k "test.zone" >>= function
+      | `Error _ -> Lwt.fail (Failure "test.zone not found")
       | `Ok sz ->
         K.read k "test.zone" 0 (Int64.to_int sz)
         >>= function
-        | `Error _ -> fail (Failure "test.zone error reading")
-        | `Ok pages -> return (String.concat "" (List.map Cstruct.to_string pages))
-    in
+        | `Error _  -> Lwt.fail (Failure "test.zone error reading")
+        | `Ok pages ->
+          Lwt.return (String.concat "" (List.map Cstruct.to_string pages))
+    end >>= fun zonebuf ->
     let open Dns_server in
     let process = process_of_zonebuf zonebuf in
     let processor = (processor_of_process process :> (module PROCESSOR)) in
@@ -43,26 +43,25 @@ module Main (C:CONSOLE) (K:KV_RO) (S:STACKV4) = struct
           Cstruct.hexdump buf;
           U.write ~source_port:listening_port ~dest_ip ~dest_port:port udp buf in
         let st, push_st = Lwt_stream.create () in
-        S.listen_udpv4 s listening_port (
-          fun ~src ~dst ~src_port buf ->
+        S.listen_udpv4 s ~port:listening_port (
+          fun ~src:_ ~dst:_ ~src_port:_ buf ->
             C.log_s c (sprintf "resolver response, length %d" (Cstruct.len buf))
             >>= fun () ->
             let ba = Cstruct.to_bigarray buf in
             push_st (Some ba);
-            return ()
+            Lwt.return_unit
         );
         let rec rxfn f =
-          Lwt_stream.get st
-          >>= function
-          | None -> fail (Failure "resolver flow closed")
+          Lwt_stream.get st >>= function
+          | None     -> Lwt.fail (Failure "resolver flow closed")
           | Some buf -> begin
               match f buf with
-              | None -> rxfn f
-              | Some r -> return r
+              | None   -> rxfn f
+              | Some r -> Lwt.return r
             end
         in
         let timerfn () = OS.Time.sleep 5.0 in
-        let cleanfn () = return () in
+        let cleanfn () = Lwt.return () in
         { Dns_resolver.txfn; rxfn; timerfn; cleanfn }
       in
       let commfn = connect_to_resolver server port in
@@ -73,7 +72,7 @@ module Main (C:CONSOLE) (K:KV_RO) (S:STACKV4) = struct
       Lwt_list.iter_s (fun ip ->
        C.log_s c (sprintf "%s -> %s" hostname (Ipaddr.to_string ip))) ips
     in
-    S.listen_udpv4 s listening_port (
+    S.listen_udpv4 s ~port:listening_port (
       fun ~src ~dst ~src_port buf ->
         C.log_s c "got udp"
         >>= fun () ->
@@ -81,8 +80,7 @@ module Main (C:CONSOLE) (K:KV_RO) (S:STACKV4) = struct
         let src' = (Ipaddr.V4 dst), listening_port in
         let dst' = (Ipaddr.V4 src), src_port in
         let obuf = (Io_page.get 1 :> Dns.Buf.t) in
-        process_query ba (Dns.Buf.length ba) obuf src' dst' processor
-        >>= function
+        process_query ba (Dns.Buf.length ba) obuf src' dst' processor >>= function
         | None ->
           C.log_s c "No response"
         | Some rba ->
