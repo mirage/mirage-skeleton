256bb4ca8ccc57d939f3ab82eed8cff4e45b474a
Thomas Leonard <talex5@gmail.com>
2016-03-13 08:25:00 +0000

    Clean up DNS example
    - Split the code into smaller functions.
    - Use logging API instead of passing a console around.
    - Add README.
    - Replace network configuration with generic_stackv4 (allows running
      with the native stack too).
    - Move hard-coded settings to top of file.
    - Comment out confusing/unlabelled hexdump.
    - Don't send client test from source port that the server is listening
      on.
    - Listen on port 53 by default. If a non-default port is used,
      `nslookup` must be run in interactive mode and in this mode it gets
      `SERVFAIL` for some reason.

--- dns/config.ml
+++ dns/config.ml
@@ -7,48 +7,13 @@ let data = crunch "./data"
     package, and it requires a logging console, a read-only
     key/value store and a TCP/IP stack. *)
 let dns_handler =
-  let libraries = ["dns.lwt-core"] in
-  let packages = ["dns"] in
+  let libraries = ["dns.lwt-core"; "mirage-logs"] in
+  let packages = ["dns"; "mirage-logs"] in
   foreign
     ~libraries ~packages
-    "Unikernel.Main" (console @-> kv_ro @-> stackv4 @-> job)
+    "Unikernel.Main" (clock @-> kv_ro @-> stackv4 @-> job)
 
-(** Supply a default console and the data to the DNS handler.
-    It still requires a network stack to become a full [job] *)
-let dns_handler_with_data =
-  dns_handler $     
-  default_console $  (* Supply the default logging console *)
-  data               (* K/V store builtin to unikernel *)
-
-(** There are two ways of building this: with pure MirageOS
-    networking or with standard Unix kernel sockets.
-    We define a "direct" key to configure this at build time. *)
-let direct_net =
-  let doc = Key.Arg.info
-    ~doc:"use direct networking if $(i,true) or kernel sockets otherwise"
-    [ "direct" ]
-  in Key.(create "direct" Arg.(opt ~stage:`Configure bool true doc))
-
-(** [direct_job] defines a job that is the pure OCaml network implementation. *)
-let direct_job =
-  dns_handler_with_data $
-  (direct_stackv4_with_dhcp default_console tap0)
-  (* MirageOS TCP/IP stack *)
-
-(* [socket_job] defines a job that uses Unix kernel sockets. *)
-let socket_job =
-  if_impl Key.is_xen
-    noop  (* Do nothing with sockets if we are in Xen *) 
-    (dns_handler_with_data $
-     socket_stackv4 default_console [Ipaddr.V4.any]) 
-
-(* If the configuration [direct_net] key is true, pick the direct
-   networking stack, otherwise select the socket *)
-let net =
-  if_impl (Key.value direct_net)
-    direct_job
-    socket_job
+let stack = generic_stackv4 default_console tap0
 
 let () =
-  register "dns" [net]
-
+  register "dns" [dns_handler $ default_clock $ data $ stack]
