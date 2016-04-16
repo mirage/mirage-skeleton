cb074d16ae559641facc200caa7c41396f8ce9de
Enguerrand Decorne <decorne.en@gmail.com>
2016-04-16 11:20:41 +0100

    Block: move generate_disk_img.sh to config.ml

--- block/config.ml
+++ block/config.ml
@@ -1,6 +1,23 @@
 open Mirage
 
-let main = foreign "Unikernel.Main" (console @-> block @-> job)
+type shellconfig = ShellConfig
+let shellconfig = Type ShellConfig
+
+let config_shell = impl @@ object
+    inherit base_configurable
+    method configure i =
+      let dir = Info.root i in
+      Functoria_app.Cmd.run "dd if=/dev/zero of=%s/disk.img count=100000" dir
+    method clean i =
+      let dir = Info.root i in
+      Functoria_app.Cmd.run "rm %s/disk.img" dir
+    method module_name = "Functoria_runtime"
+    method name = "shell_config"
+    method ty = shellconfig
+end
+
+
+let main = foreign ~deps:[abstract config_shell] "Unikernel.Main" (console @-> block @-> job)
 
 let img = block_of_file "disk.img"
 
