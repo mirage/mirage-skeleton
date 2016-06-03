b59d772bedf96d6cd4032e55c7cbea8688b37bfe
Mindy Preston <mindy.preston@docker.com>
2016-06-03 14:06:15 +0100

    `ethifv4` no longer passes console module to dhcp functor.

--- ethifv4/unikernel.ml
+++ ethifv4/unikernel.ml
@@ -13,7 +13,7 @@ module Main (C: CONSOLE) (N: NETWORK) (Clock : V1.CLOCK) = struct
   module I = Ipv4.Make(E)(A)
   module U = Udp.Make(I)
   module T = Tcp.Flow.Make(I)(OS.Time)(Clock)(Random)
-  module D = Dhcp_clientv4.Make(C)(OS.Time)(Random)(U)
+  module D = Dhcp_clientv4.Make(OS.Time)(Random)(U)
 
   let or_error _c name fn t =
     fn t
@@ -38,7 +38,7 @@ module Main (C: CONSOLE) (N: NETWORK) (Clock : V1.CLOCK) = struct
     or_error c "UDPv4" U.connect i
     >>= fun udp ->
 
-    let dhcp, _offers = D.create c (N.mac net) udp in
+    let dhcp, _offers = D.create (N.mac net) udp in
     or_error c "TCPv4" T.connect i
     >>= fun tcp ->
 
