bfb23c86c4c4078208d625082d8e15f131b3c8a3
Mindy Preston <mindy.preston@docker.com>
2016-06-13 14:37:18 +0100

    Update DHCP example to new TCPIP {un}marshallers & corresponding charrua branch

--- dhcp/config.ml
+++ dhcp/config.ml
@@ -3,9 +3,10 @@ open Mirage
 let main = foreign "Unikernel.Main" (console @-> network @-> clock @-> time @-> job)
 
 let () =
-  add_to_ocamlfind_libraries ([ "charrua-core.server";
+  add_to_ocamlfind_libraries ([ "charrua-core.server"; "tcpip.ipv4";
+                                "charrua-core.wire"; "tcpip.udp";
                                 "tcpip"; "tcpip.ethif"; "tcpip.arpv4"; "str"]);
-  add_to_opam_packages ["charrua-core"];
+  add_to_opam_packages ["charrua-core"; "tcpip"];
   register "dhcp" [
     main $ default_console $ tap0 $ default_clock $ default_time
   ]
