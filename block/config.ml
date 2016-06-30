70f758318650ee93d388ca3a20cd193b26a1dbf1
Hannes Mehnert <hannes@mehnert.org>
2016-06-30 10:31:33 +0100

    newlines are cheap, also 'rm -f' to avoid errors from file not being present

--- block/config.ml
+++ block/config.ml
@@ -5,12 +5,15 @@ let shellconfig = Type ShellConfig
 
 let config_shell = impl @@ object
     inherit base_configurable
+
     method configure i =
       let dir = Info.root i in
       Functoria_app.Cmd.run "dd if=/dev/zero of=%s/disk.img count=100000" dir
+
     method clean i =
       let dir = Info.root i in
-      Functoria_app.Cmd.run "rm %s/disk.img" dir
+      Functoria_app.Cmd.run "rm -f %s/disk.img" dir
+
     method module_name = "Functoria_runtime"
     method name = "shell_config"
     method ty = shellconfig
