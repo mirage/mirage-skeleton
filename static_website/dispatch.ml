2193e2379450acd9a9d7b9714707616335c15e3c
Richard Mortier <mort@cantab.net>
2016-07-06 17:37:18 +0100

    static_website: borrow from `static_website_tls`

--- static_website/dispatch.ml
+++ static_website/dispatch.ml
@@ -1,7 +1,7 @@
 open Lwt.Infix
 
-let www_src = Logs.Src.create "www" ~doc:"WWW server"
-module Www_log = (val Logs.src_log www_src : Logs.LOG)
+let server_src = Logs.Src.create "server" ~doc:"HTTP server"
+module Server_log = (val Logs.src_log server_src : Logs.LOG)
 
 module Main (Clock:V1.CLOCK) (FS:V1_LWT.KV_RO) (S:Cohttp_lwt.Server) = struct
   module Logs_reporter = Mirage_logs.Make(Clock)
@@ -14,20 +14,13 @@ module Main (Clock:V1.CLOCK) (FS:V1_LWT.KV_RO) (S:Cohttp_lwt.Server) = struct
       | `Error (FS.Unknown_key _) -> Lwt.fail (Failure ("read " ^ name))
       | `Ok bufs -> Lwt.return (Cstruct.copyv bufs)
 
-  let split_path uri =
-    let path = Uri.path uri in
-    let rec aux = function
-      | [] | [""] -> []
-      | hd::tl -> hd :: aux tl
-    in
-    List.filter (fun e -> e <> "")
-      (aux (Re_str.(split_delim (regexp_string "/") path)))
-
-  let rec dispatcher fs = function
-    | [] | [""] -> dispatcher fs ["index.html"]
-    | segments ->
-      let path = String.concat "/" segments in
-      Www_log.info (fun f -> f "request for '%s'" path);
+  let rec dispatcher fs uri =
+    match Uri.path uri with
+    | ("" | "/") as path ->
+      Server_log.info (fun f -> f "request for '%s'" path);
+      dispatcher fs (Uri.with_path uri "index.html")
+    | path ->
+      Server_log.info (fun f -> f "request for '%s'" path);
       Lwt.catch (fun () ->
           read_fs fs path >>= fun body ->
           let mime_type = Magic_mime.lookup path in
@@ -36,20 +29,22 @@ module Main (Clock:V1.CLOCK) (FS:V1_LWT.KV_RO) (S:Cohttp_lwt.Server) = struct
           S.respond_string ~status:`OK ~body ~headers ()
         ) (fun _exn -> S.respond_not_found ())
 
-  let start () fs http =
-     Logs.(set_level (Some Info));
-     Logs_reporter.(create () |> run) @@ fun () ->
+  let start _clock fs http =
+    Logs.(set_level (Some Info));
+    Logs_reporter.(create () |> run) @@ fun () ->
 
-     let callback _conn_id request _body =
-       let uri = Cohttp.Request.uri request in
-       dispatcher fs (split_path uri)
-     in
-     let conn_closed (_,conn_id) =
-       let cid = Cohttp.Connection.to_string conn_id in
-       Www_log.info (fun f ->f "conn %s closed" cid);
-     in
-     let port = Key_gen.port () in
-     Www_log.info (fun f -> f "WWW server listening on %d/TCP" port);
-     http (`TCP port) (S.make ~conn_closed ~callback ())
+    let callback (_, cid) request _body =
+      let uri = Cohttp.Request.uri request in
+      let cid = Cohttp.Connection.to_string cid in
+      Server_log.info (fun f -> f "[%s] serving %s" cid (Uri.to_string uri));
+      dispatcher fs uri
+    in
+    let conn_closed (_, cid) =
+      let cid = Cohttp.Connection.to_string cid in
+      Server_log.info (fun f -> f "[%s] closing" cid);
+    in
+    let port = Key_gen.port () in
+    Server_log.info (fun f -> f "listening on %d/TCP" port);
+    http (`TCP port) (S.make ~conn_closed ~callback ())
 
 end
