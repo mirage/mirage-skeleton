752405a0c5844c8b016903f1da7f078f3f4dd428
Richard Mortier <mort@cantab.net>
2016-07-06 17:53:04 +0100

    static_website_tls: tidy up and add logging

--- static_website_tls/config.ml
+++ static_website_tls/config.ml
@@ -1,32 +1,30 @@
 open Mirage
 
 let stack = generic_stackv4 default_console tap0
-
-(* storage configuration *)
-
 let data = generic_kv_ro "htdocs"
-let keys = generic_kv_ro "tls"
+let https_srv = http_server @@ conduit_direct ~tls:true stack
 
-let server =
-  foreign "Dispatch.HTTPS"
-    (http @-> kv_ro @-> kv_ro @-> clock @-> job)
+let http_port =
+  let doc = Key.Arg.info ~doc:"Listening HTTP port." ["http"] in
+  Key.(create "http_port" Arg.(opt int 8080 doc))
 
-let my_https =
-  http_server @@ conduit_direct ~tls:true stack
+let certs = generic_kv_ro "tls"
 
-let () =
+let https_port =
+  let doc = Key.Arg.info ~doc:"Listening HTTPS port." ["https"] in
+  Key.(create "https_port" Arg.(opt int 4433 doc))
+
+let main =
   let libraries = [
     "uri"; "tls"; "tls.mirage"; "mirage-http"; "mirage-logs"; "magic-mime"
   ] in
   let packages = [
     "uri"; "tls"; "mirage-http"; "mirage-logs"; "magic-mime"
   ] in
-  register "https"
-    ~packages ~libraries
-    [
-      server
-      $ my_https
-      $ data
-      $ keys
-      $ default_clock
-    ]
+  let keys = List.map Key.abstract [ http_port; https_port ] in
+  foreign
+    ~libraries ~packages ~keys
+    "Dispatch.HTTPS" (clock @-> kv_ro @-> kv_ro @-> http @-> job)
+
+let () =
+  register "https" [main $ default_clock $ data $ certs $ https_srv]
