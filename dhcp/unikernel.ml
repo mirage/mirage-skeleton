2dc058ddca3b3e35f7b5a83613a56e114ebb821e
Richard Mortier <mort@cantab.net>
2016-07-06 21:35:35 +0100

    dhcp: `ipaddr` as key; modern logging

--- dhcp/unikernel.ml
+++ dhcp/unikernel.ml
@@ -1,73 +1,77 @@
-open V1_LWT
 open Lwt.Infix
 
+let server_src = Logs.Src.create "server" ~doc:"HTTP server"
+module Server_log = (val Logs.src_log server_src : Logs.LOG)
+
 (* IP Configuration, all you need besides dhcpd.conf. *)
-let ipaddr = Ipaddr.V4.of_string_exn "192.168.1.5"
-
-
-let red fmt    = Printf.sprintf ("\027[31m"^^fmt^^"\027[m")
-let green fmt  = Printf.sprintf ("\027[32m"^^fmt^^"\027[m")
-let yellow fmt = Printf.sprintf ("\027[33m"^^fmt^^"\027[m")
-let blue fmt   = Printf.sprintf ("\027[36m"^^fmt^^"\027[m")
-
 let string_of_stream s =
   let s = List.map Cstruct.to_string s in
   (String.concat "" s)
 
-module Main (C: CONSOLE) (KV: KV_RO) (N: NETWORK) (Clock : V1.CLOCK) = struct
+module Main (Clock : V1.CLOCK) (KV: V1_LWT.KV_RO) (N: V1_LWT.NETWORK) = struct
+  module Logs_reporter = Mirage_logs.Make(Clock)
   module E = Ethif.Make(N)
   module A = Arpv4.Make(E)(Clock)(OS.Time)
-
-  let log c s =
-    Str.split_delim (Str.regexp "\n") s |>
-    List.iter (fun line -> C.log c line)
 
   let of_interest dest net =
     Macaddr.compare dest (N.mac net) = 0 || not (Macaddr.is_unicast dest)
 
-  let input_dhcp c net config leases buf =
-    let open Dhcp_server.Input in
+  let input_dhcp net config leases buf =
     match (Dhcp_wire.pkt_of_buf buf (Cstruct.len buf)) with
-    | `Error e -> log c (red "Can't parse packet: %s" e);
+    | `Error e ->
+      Server_log.warn (fun f -> f "Can't parse packet: %s" e);
       Lwt.return leases
     | `Ok pkt ->
+      let open Dhcp_server.Input in
       match (input_pkt config leases pkt (Clock.time ())) with
       | Silence -> Lwt.return leases
       | Update leases ->
-        log c (blue "Received packet %s - updated lease database" (Dhcp_wire.pkt_to_string pkt));
+        Server_log.info (fun f ->
+            let s = Dhcp_wire.pkt_to_string pkt in
+            f "Received packet %s - updated lease database" s
+          );
         Lwt.return leases
       | Warning w ->
-        log c (yellow "%s" w);
+        Server_log.warn (fun f -> f "%s" w);
         Lwt.return leases
       | Error e ->
-        log c (red "%s" e);
+        Server_log.err (fun f -> f "%s" e);
         Lwt.return leases
       | Reply (reply, leases) ->
-        log c (blue "Received packet %s" (Dhcp_wire.pkt_to_string pkt));
+        Server_log.info (fun f ->
+            let s = Dhcp_wire.pkt_to_string pkt in
+            f "Received packet %s" s
+          );
         N.write net (Dhcp_wire.buf_of_pkt reply)
         >>= fun () ->
-        log c (blue "Sent reply packet %s" (Dhcp_wire.pkt_to_string reply));
+        Server_log.info (fun f ->
+            f "Sent reply packet %s" (Dhcp_wire.pkt_to_string reply)
+          );
         Lwt.return leases
 
-  let start c kv net _ =
-    let or_error _c name fn t =
+  let start _clock kv net =
+    Logs.(set_level (Some Info));
+    Logs_reporter.(create () |> run) @@ fun () ->
+
+    let or_error name fn t =
       fn t >>= function
       | `Error _e -> Lwt.fail (Failure ("Error starting " ^ name))
       | `Ok t     -> Lwt.return t
     in
+    let ipaddr = Key_gen.ipaddr () |> Ipaddr.V4.of_string_exn in
+
     (* Read the config file *)
-    or_error c "Kv.size" (KV.size kv) "dhcpd.conf"
+    or_error "Kv.size" (KV.size kv) "dhcpd.conf"
     >>= fun size ->
-    or_error c "Kv.read" (KV.read kv "dhcpd.conf" 0) (Int64.to_int size)
+    or_error "Kv.read" (KV.read kv "dhcpd.conf" 0) (Int64.to_int size)
     >>= fun v -> Lwt.return (string_of_stream v)
     >>= fun conf ->
-    log c (green "Using configuration:");
-    log c (green "%s" conf);
+    Server_log.info (fun f -> f "Using configuration:\n%s" conf);
 
     (* Get an ARP stack *)
-    or_error c "Ethif" E.connect net
+    or_error "Ethif" E.connect net
     >>= fun e ->
-    or_error c "Arpv4" A.connect e
+    or_error "Arpv4" A.connect e
     >>= fun a ->
     A.add_ip a ipaddr
     >>= fun () ->
@@ -82,7 +86,7 @@ module Main (C: CONSOLE) (KV: KV_RO) (N: NETWORK) (Clock : V1.CLOCK) = struct
            | Some Wire_structs.ARP -> A.input a payload
            | Some Wire_structs.IPv4 ->
              if Dhcp_wire.is_dhcp buf (Cstruct.len buf) then
-               input_dhcp c net config !leases buf >>= fun new_leases ->
+               input_dhcp net config !leases buf >>= fun new_leases ->
                leases := new_leases;
                Lwt.return_unit
              else
