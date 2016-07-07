d224edf8fd16a7ab7ffa5bc3e017ebdd843e9ac4
Mindy Preston <mindy.preston@docker.com>
2016-07-07 17:16:38 +0100

    unbreaks http-fetch config

--- http-fetch/config.ml
+++ http-fetch/config.ml
@@ -8,7 +8,7 @@ let client =
     "Unikernel.Client" @@ time @-> console @-> resolver @-> conduit @-> job
 
 let () =
-  let stack = generic_stackv4 tap0
+  let stack = generic_stackv4 tap0 in
   let res_dns = resolver_dns stack in
   let conduit = conduit_direct stack in
   let job =  [ client $ default_time $ default_console $ res_dns $ conduit ] in
