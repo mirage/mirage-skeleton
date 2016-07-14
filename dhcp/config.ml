69df0d5d50bead0a060f99ed7a1bd4fda4695ef0
Mindy Preston <mindy.preston@docker.com>
2016-07-14 16:13:29 +0100

    replaces deprecated pkg/lib configure functions in dhcp

--- dhcp/config.ml
+++ dhcp/config.ml
@@ -3,10 +3,10 @@ open Mirage
 let main = foreign "Unikernel.Main" (console @-> network @-> clock @-> time @-> job)
 
 let () =
-  add_to_ocamlfind_libraries ([ "charrua-core.server"; "tcpip.ipv4";
-                                "charrua-core.wire"; "tcpip.udp";
-                                "tcpip"; "tcpip.ethif"; "tcpip.arpv4"; "str"]);
-  add_to_opam_packages ["charrua-core"; "tcpip"];
-  register "dhcp" [
+  let libraries = [ "charrua-core.server"; "tcpip.ipv4";
+                    "charrua-core.wire"; "tcpip.udp";
+                    "tcpip"; "tcpip.ethif"; "tcpip.arpv4"; "str"] in
+  let packages = ["charrua-core"; "tcpip"] in
+  register "dhcp" ~libraries ~packages [
     main $ default_console $ tap0 $ default_clock $ default_time
   ]
