7fff6bca29c30760d6c5560e176f0ca8eafc5d5f
Hannes Mehnert <hannes@mehnert.org>
2016-07-28 18:59:09 +0200

    revert dependency on dns.lwt-core, use dns.mirage

--- dns/config.ml
+++ dns/config.ml
@@ -7,7 +7,7 @@ let data = crunch "./data"
     package, and it requires a logging console, a read-only
     key/value store and a TCP/IP stack. *)
 let dns_handler =
-  let libraries = ["dns.lwt-core"; "mirage-logs"; "duration"] in
+  let libraries = ["dns.mirage"; "mirage-logs"; "duration"] in
   let packages = ["dns"; "mirage-logs"; "duration"] in
   foreign
     ~libraries ~packages
