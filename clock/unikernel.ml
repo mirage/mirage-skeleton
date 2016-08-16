110594b611863085f61db003161397f0996c022d
Mindy Preston <mindy.preston@docker.com>
2016-08-16 17:48:22 -0500

    include monotonic clock too

--- clock/unikernel.ml
+++ clock/unikernel.ml
@@ -1,12 +1,12 @@
-module Main (Console : V1_LWT.CONSOLE) (PClock : V1_LWT.PCLOCK) = struct
+module Main (Console : V1_LWT.CONSOLE) (PClock : V1_LWT.PCLOCK) (MClock : V1_LWT.MCLOCK) = struct
   let str_of_time (posix_time, timezone) =
     Format.asprintf "%a" (Ptime.pp_human ?tz_offset_s:timezone ()) posix_time
 
-  let start console pclock =
+  let start console pclock mclock =
     let rec speak () =
       let time = PClock.now_d_ps pclock |> Ptime.v in
       let tz = PClock.current_tz_offset_s pclock in
-      Console.log console (Printf.sprintf "At the chime, the time will be %s. \b" @@ str_of_time (time, tz));
+      Console.log console (Printf.sprintf "%Lu nanoseconds have elapsed. At the chime, the time will be %s. \b *DING*" (Mclock.elapsed_ns mclock) @@ str_of_time (time, tz));
       speak ()
     in
     speak ()
