a15ce48c4284691bf2b370cfe2506b0197c5c0bf
Mindy Preston <mindy.preston@docker.com>
2016-11-02 08:29:33 -0500

    ipv4 config changes

--- netif-forward/config.ml
+++ netif-forward/config.ml
@@ -3,6 +3,6 @@ open Mirage
 let main = foreign "Unikernel.Main" (console @-> network @-> network @-> job)
 
 let () =
-  register "network" [
+        register "network" ~packages:["rresult"] ~libraries:["rresult"] [
     main $ default_console $ (netif "1") $ (netif ~group:"other" "2")
   ]
