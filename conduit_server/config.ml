3ab4dc8fbb5dc18bdfb97826222bca0c1ccf2a8b
Mindy Preston <meetup@yomimono.org>
2016-11-02 09:51:00 -0500

    modernize conduit example
    Replace calls to Console.log_s with Log.debug , and remove custom
    config.ml code for choosing the stack based on the target.  Use mirage's
    generic_stackv4 instead and rely on Functoria's key mechanisms.

--- conduit_server/config.ml
+++ conduit_server/config.ml
@@ -5,19 +5,7 @@ let handler =
   let packages = ["mirage-http"] in
   foreign
     ~libraries ~packages
-    "Unikernel.Main" (console @-> conduit @-> job)
-
-let direct =
-  let stack = direct_stackv4_with_default_ipv4 tap0 in
-  let server = conduit_direct stack in
-  handler $ default_console $ server
-
-(* Only add the Unix socket backend if the configuration mode is Unix *)
-let socket =
-  let c = default_console in
-  if_impl Key.is_unix
-    (handler $ c $ conduit_direct (socket_stackv4 [Ipaddr.V4.any]))
-    noop
+    "Unikernel.Main" (conduit @-> job)
 
 let () =
-  register "conduit_server" [direct ; socket ]
+  register "conduit_server" [ handler $ conduit_direct (generic_stackv4 tap0) ]
