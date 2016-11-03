a835094e4657b37b303a0751bd8304676e268ff0
Mindy Preston <mindy.preston@docker.com>
2016-11-03 18:06:14 -0500

    use generic_stackv4 instead of default_stack_with_default_ipv4

--- tracing/config.ml
+++ tracing/config.ml
@@ -1,7 +1,7 @@
 open Mirage
 
 let main = foreign "Unikernel.Main" (stackv4 @-> job)
-let stack = direct_stackv4_with_default_ipv4 tap0
+let stack = generic_stackv4 tap0
 
 let tracing = mprof_trace ~size:1000000 ()
 
