1ee364a5a8882aab9e21fa88aa997a19b80ce92f
Richard Mortier <mort@cantab.net>
2017-01-04 23:46:53 +0000

    clock: reinvigorate speaking clock, tidy `.gitignore`

--- clock/unikernel.ml
+++ clock/unikernel.ml
@@ -1,20 +1,33 @@
 open Lwt.Infix
 
-module Main (Console : V1_LWT.CONSOLE) (Time : V1_LWT.TIME) (PClock : V1_LWT.PCLOCK) (MClock : V1_LWT.MCLOCK) = struct
+let log = Logs.Src.create "speaking clock" ~doc:"At the third stroke..."
+module Log = (val Logs.src_log log : Logs.LOG)
+
+module Main (Time: V1.TIME) (PClock: V1.PCLOCK) (MClock: V1.MCLOCK) = struct
+
+  module Logs_reporter = Mirage_logs.Make(PClock)
+
   let str_of_time (posix_time, timezone) =
     Format.asprintf "%a" (Ptime.pp_human ?tz_offset_s:timezone ()) posix_time
 
-  let start console _ pclock mclock =
+  let start _time pclock mclock =
+
+    Logs.(set_level (Some Info));
+    Logs_reporter.(create pclock |> run) @@ fun () ->
+
     let rec speak pclock mclock () =
       let current_time = PClock.now_d_ps pclock |> Ptime.v in
       let tz = PClock.current_tz_offset_s pclock in
       let str =
-        Printf.sprintf "%Lu nanoseconds have elapsed. At the chime, the time will be %s. \x08 *DING*"
+        Printf.sprintf
+          "%Lu nanoseconds have elapsed. \n\
+          \ At the stroke, the time will be %s \x07 *BEEP*"
           (Mclock.elapsed_ns mclock) @@ str_of_time (current_time, tz)
       in
-      Console.log console str >>= fun () ->
-      Time.sleep_ns 1_000_000_000L >>= fun () ->
+      Log.info (fun f -> f "%s" str);
+      OS.Time.sleep_ns 1_000_000_000L >>= fun () ->
       speak pclock mclock ()
     in
     speak pclock mclock ()
+
 end
