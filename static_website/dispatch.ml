ca1ac49a83f88103214745ab011d391b42bdd96a
Anil Madhavapeddy <anil@recoil.org>
2017-02-02 17:44:18 +0000

    fix

--- static_website/dispatch.ml
+++ static_website/dispatch.ml
@@ -1,3 +1,4 @@
+let server_src = Logs.Src.create "server" ~doc:"HTTP server"
 module Server_log = (val Logs.src_log server_src : Logs.LOG)
 
 module Main (FS:Mirage_types_lwt.KV_RO) (S:Cohttp_lwt.Server) = struct
