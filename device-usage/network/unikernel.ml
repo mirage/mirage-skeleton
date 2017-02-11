d04ab76783d54808c324d4e6d086655798e8cdfa
Mindy Preston <mindy.preston@docker.com>
2017-02-10 18:01:06 -0600

    logify network example

--- device-usage/network/unikernel.ml
+++ device-usage/network/unikernel.ml
@@ -1,24 +1,19 @@
 open Lwt.Infix
 
-let red fmt    = Fmt.strf ("\027[31m"^^fmt^^"\027[m")
-let green fmt  = Fmt.strf ("\027[32m"^^fmt^^"\027[m")
-let yellow fmt = Fmt.strf ("\027[33m"^^fmt^^"\027[m")
-let blue fmt   = Fmt.strf ("\027[36m"^^fmt^^"\027[m")
+module Main (S: Mirage_stack_lwt.V4) = struct
 
-module Main (C: Mirage_types_lwt.CONSOLE) (S: Mirage_types_lwt.STACKV4) = struct
-
-  let start c s =
-    S.listen_tcpv4 s ~port:8080 (fun flow ->
+  let start s =
+    let port = Key_gen.port () in
+    S.listen_tcpv4 s ~port (fun flow ->
         let dst, dst_port = S.TCPV4.dst flow in
-        C.log c (green "new tcp connection from %s %d"
-                   (Ipaddr.V4.to_string dst) dst_port) >>= fun () ->
+        Logs.info (fun f -> f "new tcp connection from IP %s on port %d"
+                  (Ipaddr.V4.to_string dst) dst_port);
         S.TCPV4.read flow >>= function
+        | Ok `Eof -> Logs.info (fun f -> f "Closing connection!"); Lwt.return_unit
+        | Error e -> Logs.warn (fun f -> f "Error reading data from established connection: %a" S.TCPV4.pp_error e); Lwt.return_unit
         | Ok (`Data b) ->
-          C.log c (yellow "read: %d\n%s" (Cstruct.len b) (Cstruct.to_string b))
-          >>= fun () ->
+          Logs.debug (fun f -> f "read: %d bytes:\n%s" (Cstruct.len b) (Cstruct.to_string b));
           S.TCPV4.close flow
-        | Ok `Eof -> C.log c (green "read: eof")
-        | Error e -> C.log c (red "read: error %a" S.TCPV4.pp_error e)
       );
 
     S.listen s
