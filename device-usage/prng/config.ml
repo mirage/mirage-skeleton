c6a7d0a908133a56aaf57ecf7140bf89db4a489f
Hannes Mehnert <hannes@mehnert.org>
2017-02-11 17:49:10 +0000

    require nocrypto.mirage by requiring th packages it needs to be built

--- device-usage/prng/config.ml
+++ device-usage/prng/config.ml
@@ -6,6 +6,9 @@ let () =
   let packages = [
     package "randomconv" ;
     package "mirage-entropy" ;
+    (* access to entropy sources is only via Nocrypto_entropy_mirage.sources,
+       which is only compiled when nocrypto uses xen or freestanding flags *)
+    package ~ocamlfind:[] "ocaml-freestanding" ;
     package ~sublibs:["mirage"] "nocrypto"
   ] in
   register ~packages "prng" [main $ default_random]
