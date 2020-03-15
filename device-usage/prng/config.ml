ce5412de3bcf923ce9ee7e1ce1d02b7d8e51fced
Hannes Mehnert <hannes@mehnert.org>
2020-03-15 13:55:40 +0100

    update prng for new mirage-crypto mirage

--- device-usage/prng/config.ml
+++ device-usage/prng/config.ml
@@ -1,10 +1,10 @@
 open Mirage
 
-let main = foreign ~deps:[abstract nocrypto] "Unikernel.Main" (random @-> job)
+let main = foreign "Unikernel.Main" (random @-> job)
 
 let () =
   let packages = [
     package "randomconv" ;
-    package "mirage-entropy" ;
+    package "mirage-crypto-entropy" ;
   ] in
   register ~packages "prng" [main $ default_random]
