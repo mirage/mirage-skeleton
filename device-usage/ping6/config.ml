223faab8dbecf555a4e0a209a64364b87c311959
Hannes Mehnert <hannes@mehnert.org>
2020-12-02 18:39:39 +0100

    mirage 3.10 support

--- device-usage/ping6/config.ml
+++ device-usage/ping6/config.ml
@@ -5,13 +5,7 @@ let main =
     "Unikernel.Main" (console @-> network @-> ethernet @-> ipv6 @-> job)
 let net = default_network
 let ethif = etif net
-let ipv6 =
-  let config = {
-    addresses = [];
-    netmasks  = [];
-    gateways  = [];
-  } in
-  create_ipv6 ethif config
+let ipv6 = create_ipv6 net ethif
 
 let () =
   register "ping" [ main $ default_console $ default_network $ ethif $ ipv6 ]
