d0495add1f5c1ae2c6a21a031f7934dafd40bfc4
Lucas Pluvinage <lucas@tarides.com>
2021-02-18 17:43:08 +0100

    Fix device-usage/block unikernel

--- device-usage/block/config.ml
+++ device-usage/block/config.ml
@@ -1,28 +1,23 @@
 open Mirage
 
 type shellconfig = ShellConfig
-let shellconfig = Type ShellConfig
+let shellconfig = typ ShellConfig
 
-let config_shell = impl @@ object
-    inherit base_configurable
 
-    method! build _i =
-      Bos.OS.Cmd.run Bos.Cmd.(v "dd" % "if=/dev/zero" % "of=disk.img" % "count=100000")
-
-    method! clean _i =
-      Bos.OS.File.delete (Fpath.v "disk.img")
-
-    method module_name = "Functoria_runtime"
-    method name = "shell_config"
-    method ty = shellconfig
-end
-
+let config_shell = 
+  let build _i = 
+    Action.run_cmd Bos.Cmd.(v "dd" % "if=/dev/zero" % "of=disk.img" % "count=100000")
+  in
+  let clean _i =
+    Action.rm (Fpath.v "disk.img")
+  in
+  impl ~build ~clean "shell_config" shellconfig
 
 let main =
   let packages = [ package "io-page"; package "duration"; package ~build:true "bos"; package ~build:true "fpath" ] in
   foreign
     ~packages
-    ~deps:[abstract config_shell] "Unikernel.Main" (time @-> block @-> job)
+    ~deps:[dep config_shell] "Unikernel.Main" (time @-> block @-> job)
 
 let img = Key.(if_impl is_solo5 (block_of_file "storage") (block_of_file "disk.img"))
 
