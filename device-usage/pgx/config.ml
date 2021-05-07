1428f697173063d8f913c4b4a1b9ef7855c48c7f
Lucas Pluvinage <lucas@tarides.com>
2021-05-07 11:09:17 +0200

    pgx: adapt to 2.0 changes

--- device-usage/pgx/config.ml
+++ device-usage/pgx/config.ml
@@ -1,11 +1,12 @@
 open Mirage
 
 let packages =
-  [ package "pgx"
+  [ package ~min:"2.0" "pgx"
   ; package "pgx_lwt"
   ; package "pgx_lwt_mirage" ~min:"2.0"
   ; package "logs"
   ; package "mirage-logs"
+  ; package ~max:"2.2.0" "conduit" (* force conduit-mirage and conduit to have the same version. *)
   ]
 ;;
 
