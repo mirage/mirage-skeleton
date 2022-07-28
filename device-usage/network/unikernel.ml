f61e6aae92058293e2c5b1d7307a50370eb6ad3a
Hannes Mehnert <hannes@mehnert.org>
2022-07-28 22:37:31 +0200

    use stackv4v6 instead of stackv4

--- device-usage/network/unikernel.ml
+++ device-usage/network/unikernel.ml
@@ -1,26 +1,26 @@
 open Lwt.Infix
 
-module Main (S : Tcpip.Stack.V4) = struct
+module Main (S : Tcpip.Stack.V4V6) = struct
   let start s =
     let port = Key_gen.port () in
-    S.TCPV4.listen (S.tcpv4 s) ~port (fun flow ->
-        let dst, dst_port = S.TCPV4.dst flow in
+    S.TCP.listen (S.tcp s) ~port (fun flow ->
+        let dst, dst_port = S.TCP.dst flow in
         Logs.info (fun f ->
             f "new tcp connection from IP %s on port %d"
-              (Ipaddr.V4.to_string dst) dst_port);
-        S.TCPV4.read flow >>= function
+              (Ipaddr.to_string dst) dst_port);
+        S.TCP.read flow >>= function
         | Ok `Eof ->
             Logs.info (fun f -> f "Closing connection!");
             Lwt.return_unit
         | Error e ->
             Logs.warn (fun f ->
                 f "Error reading data from established connection: %a"
-                  S.TCPV4.pp_error e);
+                  S.TCP.pp_error e);
             Lwt.return_unit
         | Ok (`Data b) ->
             Logs.debug (fun f ->
                 f "read: %d bytes:\n%s" (Cstruct.length b) (Cstruct.to_string b));
-            S.TCPV4.close flow);
+            S.TCP.close flow);
 
     S.listen s
 end
--- device-usage/tracing/config.ml
+++ device-usage/tracing/config.ml
@@ -1,6 +1,6 @@
 open Mirage
 
-let main = main "Unikernel.Main" (stackv4 @-> job)
-let stack = generic_stackv4 default_network
+let main = main "Unikernel.Main" (stackv4v6 @-> job)
+let stack = generic_stackv4v6 default_network
 let tracing = mprof_trace ~size:1000000 ()
 let () = register "example" ~tracing [ main $ stack ]
