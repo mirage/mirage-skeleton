9e7c2b23852f3c32c918aaa3d3086528f0de1d2a
Hannes Mehnert <hannes@mehnert.org>
2023-04-08 18:19:54 +0200

    applications/crypto and device-usage/prng: explicitly require fmt

--- device-usage/prng/config.ml
+++ device-usage/prng/config.ml
@@ -3,7 +3,10 @@ open Mirage
 let main = main "Unikernel.Main" (random @-> job)
 
 let () =
-  let packages =
-    [ package "randomconv"; package ~min:"0.7.0" "mirage-crypto-rng" ]
+  let packages = [
+    package "randomconv";
+    package ~min:"0.7.0" "mirage-crypto-rng";
+    package "fmt";
+  ]
   in
   register ~packages "prng" [ main $ default_random ]
