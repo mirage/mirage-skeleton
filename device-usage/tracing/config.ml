d274621accdc287337cc95ca849641de523253ef
Hannes Mehnert <hannes@mehnert.org>
2023-06-26 13:05:21 +0200

    remove tracing (which was removed in mirage 4.4.0

--- device-usage/tracing/config.ml
+++
@@ -1,6 +0,0 @@
-open Mirage
-
-let main = main "Unikernel.Main" (stackv4v6 @-> job)
-let stack = generic_stackv4v6 default_network
-let tracing = mprof_trace ~size:1000000 ()
-let () = register "example" ~tracing [ main $ stack ]
