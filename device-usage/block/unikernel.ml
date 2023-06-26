2da1d2bb4733738c6e1766d4ba23f89309d20659
Reynir Björnsson <reynir@reynir.dk>
2023-06-26 16:23:59 +0200

    dune build @fmt

--- device-usage/block/unikernel.ml
+++ device-usage/block/unikernel.ml
@@ -18,23 +18,20 @@ module Main (B : Mirage_block.S) = struct
 
   let rec fill_with_pattern x phrase =
     assert (String.length phrase > 0);
-    if Cstruct.length x > 0 then begin
+    if Cstruct.length x > 0 then (
       let l = min (String.length phrase) (Cstruct.length x) in
       Cstruct.blit_from_string phrase 0 x 0 l;
-      fill_with_pattern (Cstruct.shift x l) phrase
-    end
+      fill_with_pattern (Cstruct.shift x l) phrase)
 
   let alloc_dull_boy sector_size n =
     let b = Cstruct.create_unsafe (n * sector_size) in
-    List.init
-      n
-      (fun i ->
-          let sector = Cstruct.sub b (i * sector_size) sector_size in
-          let phrase =
-            sprintf "%d: All work and no play makes Dave a dull boy.\n" i
-          in
-          fill_with_pattern sector phrase;
-          sector)
+    List.init n (fun i ->
+        let sector = Cstruct.sub b (i * sector_size) sector_size in
+        let phrase =
+          sprintf "%d: All work and no play makes Dave a dull boy.\n" i
+        in
+        fill_with_pattern sector phrase;
+        sector)
 
   let check_sector_write b offset length =
     Log.debug (fun f -> f "writing %d sector(s) at %Ld\n" length offset);
@@ -42,21 +39,22 @@ module Main (B : Mirage_block.S) = struct
     B.get_info b >>= fun info ->
     let sectors = alloc_dull_boy info.sector_size length in
     B.write b offset sectors >>= fun r ->
-    begin match r with
-      | Ok () -> ()
-      | Error e ->
-        Log.err (fun m -> m "%a" B.pp_write_error e); exit 2
-    end;
+    (match r with
+    | Ok () -> ()
+    | Error e ->
+        Log.err (fun m -> m "%a" B.pp_write_error e);
+        exit 2);
     let sectors' =
       let b = Cstruct.create (length * info.sector_size) in
-      List.init length (fun i -> Cstruct.sub b (i * info.sector_size) info.sector_size)
+      List.init length (fun i ->
+          Cstruct.sub b (i * info.sector_size) info.sector_size)
     in
     B.read b offset sectors' >>= fun r ->
-    begin match r with
-      | Ok () -> ()
-      | Error e ->
-        Log.err (fun m -> m "%a" B.pp_error e); exit 2
-    end;
+    (match r with
+    | Ok () -> ()
+    | Error e ->
+        Log.err (fun m -> m "%a" B.pp_error e);
+        exit 2);
     List.iter2 (fun a b -> check_equal a b) sectors sectors';
     incr tests_passed;
     Lwt.return_unit
@@ -78,7 +76,7 @@ module Main (B : Mirage_block.S) = struct
     printf "reading %d sectors at %Ld\n%!" length offset;
     incr tests_started;
     B.get_info b >>= fun info ->
-    let sectors = [Cstruct.create (length * info.sector_size)] in
+    let sectors = [ Cstruct.create (length * info.sector_size) ] in
     Log.debug (fun f ->
         f "Expecting error output from the following operation...");
     B.read b offset sectors >|= function
@@ -92,24 +90,19 @@ module Main (B : Mirage_block.S) = struct
     Log.info (fun f -> f "%a" Mirage_block.pp_info info);
 
     check_sector_write b 0L 1 >>= fun () ->
-    check_sector_write b (Int64.sub info.size_sectors 1L) 1
-    >>= fun () ->
+    check_sector_write b (Int64.sub info.size_sectors 1L) 1 >>= fun () ->
     check_sector_write b 0L 2 >>= fun () ->
-    check_sector_write b (Int64.sub info.size_sectors 2L) 2
-    >>= fun () ->
+    check_sector_write b (Int64.sub info.size_sectors 2L) 2 >>= fun () ->
     check_sector_write b 0L 12 >>= fun () ->
-    check_sector_write b (Int64.sub info.size_sectors 12L) 12
-    >>= fun () ->
-    check_sector_write_failure b info.size_sectors 1
-    >>= fun () ->
+    check_sector_write b (Int64.sub info.size_sectors 12L) 12 >>= fun () ->
+    check_sector_write_failure b info.size_sectors 1 >>= fun () ->
     check_sector_write_failure b (Int64.sub info.size_sectors 11L) 12
     >>= fun () ->
-    check_sector_read_failure b info.size_sectors 1
-    >>= fun () ->
+    check_sector_read_failure b info.size_sectors 1 >>= fun () ->
     check_sector_read_failure b (Int64.sub info.size_sectors 11L) 12
     >|= fun () ->
     Log.info (fun f -> f "Test sequence finished\n");
     Log.info (fun f -> f "Total tests started: %d\n" !tests_started);
     Log.info (fun f -> f "Total tests passed:  %d\n" !tests_passed);
-    Log.info (fun f -> f "Total tests failed:  %d\n%!" !tests_failed);
+    Log.info (fun f -> f "Total tests failed:  %d\n%!" !tests_failed)
 end
