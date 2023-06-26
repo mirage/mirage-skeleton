36d5f38a5eed058a97ad16e3950a69efc7d72228
Reynir Björnsson <reynir@reynir.dk>
2023-06-26 12:33:05 +0200

    Further simplify device-usage/block

--- device-usage/block/config.ml
+++ device-usage/block/config.ml
@@ -1,15 +1,8 @@
 open Mirage
 
-let main =
-  let packages =
-    [
-      package "io-page";
-      package "duration";
-    ]
-  in
-  main ~packages "Unikernel.Main" (block @-> job)
+let main = main "Unikernel.Main" (block @-> job)
 
 let img =
-  Key.(if_impl is_solo5 (block_of_file "storage") (block_of_file "disk.img"))
+  if_impl Key.is_solo5 (block_of_file "storage") (block_of_file "disk.img")
 
 let () = register "block_test" [ main $ img ]
