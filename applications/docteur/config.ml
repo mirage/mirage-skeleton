bb1ccbf39654d347775937027f8b449b542714f3
Thomas Gazagnaire <thomas@gazagnaire.org>
2023-06-29 16:04:18 +0200

    docteur: filename is a runtime parameter

--- applications/docteur/config.ml
+++ applications/docteur/config.ml
@@ -2,7 +2,7 @@ open Mirage
 
 let filename =
   let doc = Key.Arg.info ~doc:"The filename to print out." [ "filename" ] in
-  Key.(create "filename" Arg.(required string doc))
+  Key.(create "filename" Arg.(required ~stage:`Run string doc))
 
 let unikernel = foreign "Unikernel.Make" ~keys:[ Key.v filename ] (kv_ro @-> job)
 let remote = "https://github.com/mirage/mirage"
