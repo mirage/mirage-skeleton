c7841e2796270c3114cbff932e3f7a0ff86b62c4
Reynir Björnsson <reynir@reynir.dk>
2023-06-29 16:17:55 +0200

    dune build @fmt

--- tutorial/hello/config.ml
+++ tutorial/hello/config.ml
@@ -1,4 +1,6 @@
 open Mirage
 
-let main = main "Unikernel.Hello" (time @-> job) ~packages:[ package "duration" ]
+let main =
+  main "Unikernel.Hello" (time @-> job) ~packages:[ package "duration" ]
+
 let () = register "hello" [ main $ default_time ]
