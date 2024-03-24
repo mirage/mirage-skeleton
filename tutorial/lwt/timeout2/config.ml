e699c5c5f5772cc91d040d7442d78481fade2f6d
oemmerson <oemmerson@gmx.com>
2024-03-24 09:09:34 +0000

    Update tutorial/lwt/timeout2/config.ml
    Max is exclusive.
    
    Co-authored-by: Reynir Björnsson <reynir@reynir.dk>

--- tutorial/lwt/timeout2/config.ml
+++ tutorial/lwt/timeout2/config.ml
@@ -2,7 +2,7 @@ open Mirage
 
 let main =
   main
-    ~packages:[ package "duration"; package ~max:"0.1.3" "randomconv" ]
+    ~packages:[ package "duration"; package ~max:"0.2.0" "randomconv" ]
     "Unikernel.Timeout2"
     (time @-> random @-> job)
 
