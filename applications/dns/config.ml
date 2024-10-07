64428477981a4477430c20f632caea9c2401dee5
Hannes Mehnert <hannes@mehnert.org>
2024-10-07 18:18:39 +0200

    adjust unikernels

--- applications/dns/config.ml
+++ applications/dns/config.ml
@@ -1,16 +1,10 @@
 (* mirage >= 4.6.0 & < 4.8.0 *)
 open Mirage
 
-let timeout = Runtime_arg.create ~pos:__POS__ "Unikernel.timeout"
-let nameservers = Runtime_arg.create ~pos:__POS__ "Unikernel.nameservers"
 let runtime_args = [ runtime_arg ~pos:__POS__ "Unikernel.domain_name" ]
 let unikernel = main ~runtime_args "Unikernel.Make" (dns_client @-> job)
 let stackv4v6 = generic_stackv4v6 default_network
 let happy_eyeballs = generic_happy_eyeballs stackv4v6
 
 let () =
-  register "resolve"
-    [
-      unikernel
-      $ generic_dns_client ~timeout ~nameservers stackv4v6 happy_eyeballs;
-    ]
+  register "resolve" [ unikernel $ generic_dns_client stackv4v6 happy_eyeballs ]
--- applications/dns/unikernel.ml
+++ applications/dns/unikernel.ml
@@ -1,13 +1,5 @@
 open Cmdliner
 
-let nameservers =
-  let doc = Arg.info ~doc:"Nameserver." [ "nameserver" ] in
-  Arg.(value & opt_all string [] doc)
-
-let timeout =
-  let doc = Arg.info ~doc:"Timeout of DNS requests." [ "timeout" ] in
-  Arg.(value & opt (some int64) None doc)
-
 let domain_name =
   let doc = Arg.info ~doc:"The domain-name to resolve." [ "domain-name" ] in
   Arg.(required & opt (some string) None doc)
