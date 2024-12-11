18fa4b2287580d8caed3c26e3c0ffa1350c02a9f
Hannes Mehnert <hannes@mehnert.org>
2024-12-11 18:10:43 +0100

    fix with charrua 1.6.0 release

--- applications/dhcp/config.ml
+++ applications/dhcp/config.ml
@@ -3,7 +3,7 @@ open Mirage
 
 let packages =
   [
-    package ~min:"1.0.0" "charrua";
+    package ~min:"1.6.0" "charrua";
     package "charrua-server";
     package ~min:"3.0.0" ~sublibs:[ "mirage" ] "arp";
     package ~min:"3.0.0" "ethernet";
--- applications/dhcp/unikernel.ml
+++ applications/dhcp/unikernel.ml
@@ -24,8 +24,8 @@ struct
         | Silence -> Lwt.return leases
         | Update leases ->
             Logs.info (fun m ->
-                m "Received packet %s - updated lease database"
-                  (Dhcp_wire.pkt_to_string pkt));
+                m "Received packet %a - updated lease database"
+                  Dhcp_wire.pp_pkt pkt);
             Lwt.return leases
         | Warning w ->
             Logs.warn (fun m -> m "%s" w);
@@ -35,13 +35,13 @@ struct
             Lwt.return leases
         | Reply (reply, leases) ->
             Logs.info (fun m ->
-                m "Received packet %s" (Dhcp_wire.pkt_to_string pkt));
+                m "Received packet %a" Dhcp_wire.pp_pkt pkt);
             N.write net
               ~size:(N.mtu net + Ethernet.Packet.sizeof_ethernet)
               (Dhcp_wire.pkt_into_buf reply)
             >>= fun _ ->
             Logs.info (fun m ->
-                m "Sent reply packet %s" (Dhcp_wire.pkt_to_string reply));
+                m "Sent reply packet %a" Dhcp_wire.pp_pkt reply);
             Lwt.return leases)
 
   let start net clock _time =
