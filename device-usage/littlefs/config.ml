7f254c860367a85f348fea6c5da45539c2d4f2e3
yomimono <maintenance@identity-function.com>
2025-10-22 21:16:22 +0200

    device-usage/littlefs: update; use cleartext
    mirage-block-ccm has not been updated for mirage-crypto > 1.0.0,
    so drop the use of the encrypted block device and just run the
    read-write filesystem directly on top of the cleartext block device.

--- device-usage/littlefs/config.ml
+++ device-usage/littlefs/config.ml
@@ -1,23 +1,13 @@
-(* mirage >= 4.4.0 & < 4.5.0 *)
+(* mirage >= 4.10.0 & < 4.11.0 *)
 open Mirage
 
 let unikernel =
   main "Unikernel.Make"
     ~packages:[ package "hxd" ~sublibs:[ "core"; "string" ] ]
-    (random @-> kv_rw @-> job)
-
-let aes_ccm_key =
-  let doc =
-    Key.Arg.info [ "aes-ccm-key" ]
-      ~doc:"The key of the block device (hex formatted)"
-  in
-  Key.(create "aes-ccm-key" Arg.(required string doc))
-
-let program_block_size =
-  let doc = Key.Arg.info [ "program-block-size" ] in
-  Key.(create "program_block_size" Arg.(opt int 16 doc))
+    (kv_rw @-> job)
 
 let block = block_of_file "littlefs"
-let encrypted_block = ccm_block aes_ccm_key block
-let fs = chamelon ~program_block_size encrypted_block
-let () = register "elittlefs" [ unikernel $ default_random $ fs ]
+let program_block_size =
+  Runtime_arg.create ~pos:__POS__ "Unikernel.program_block_size"
+let fs = chamelon ~program_block_size block
+let () = register "elittlefs" [ unikernel $ fs ]
